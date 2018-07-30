42b3f888245da760dd37d09184fbc0a5eb05bb64
Jayanth Koroth <koro7020@mylaurier.ca>
2018-07-30 11:08:05 -0400

    Added the login screen to the app

---
+++ app/src/main/java/com/shout/android/Login2Activity.java
@@ -0,0 +1,13 @@
+package com.shout.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
